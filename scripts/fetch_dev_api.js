3e4a08002d6ba8ea50085fd6d46ec480f57fb3a5
NorthStarVTC <odamalfie@gmail.com>
2026-01-30 13:10:30 +0000

    Delete scripts directory

--- scripts/fetch_dev_api.js
+++
@@ -1,8 +0,0 @@
-const http = require('http');
-const url = 'http://localhost:5173/api/gallery/events';
-http.get(url, res=>{
-  console.log('status', res.statusCode);
-  let data='';
-  res.on('data', c=>data+=c);
-  res.on('end', ()=>{ console.log('body:', data); });
-}).on('error', e=>console.error('error', e.message));
